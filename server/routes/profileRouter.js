bd42476b522b6014653aba65a6fa85a663ddda46
Prachi Bodhe <prachibodhe08@gmail.com>
2026-07-27 22:46:00 +0530

    Fix profile router import

--- server/routes/profileRouter.js
+++ server/routes/profileRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Profile = require("../models/Profile");
+const Profile = require("../models/profile");
 
 router.get("/", (req, res) => {
   res.send("Profile route working");
