c453c081169369ebe75184c3ee08daee374ed48b
Prachi Bodhe <prachibodhe08@gmail.com>
2026-07-28 22:00:57 +0530

    Fix contact API URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ const sendMessage = async () => {
   try {
     console.log("Send clicked");
 
-    const response = await fetch("http://localhost:5000/contact", {
+    const response = await fetch("https://codefolio1.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
